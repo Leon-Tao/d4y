7e320d1d369fa7e624d4a043ae193c9617f21609
GitHub Action <action@github.com>
2024-03-27 13:57:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/leontao/d4y/MyBundle.kt
+++ src/main/kotlin/com/github/leontao/d4y/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.leontao.d4y
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/leontao/d4y/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/leontao/d4y/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.leontao.d4y.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
